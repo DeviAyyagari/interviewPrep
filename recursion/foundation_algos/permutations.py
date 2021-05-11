b4e1226bc9e699cee7ee6bf5d4c0f21d1bf2bae5
DeviAyyagari <akdevi.92@gmail.com>
2021-05-10 20:56:57 -0700

    fixing commits

---
+++ recursion/foundation_algos/permutations.py
@@ -0,0 +1,34 @@
+"""
+Problem statement: Given a string with n distince characters, print all
+                    permutations of the string.
+Intuition: At the first level of recursive tree, there are n choices for the node
+            to make. Each node passes down the partial solutions
+            and the reduced "domain" to the child node.
+            At the second level, each of the n nodes have (n-1) choices to make.
+            Leaf nodes will have solution.
+Space complexity: At any point in time only ine leaf nodes' tree structure
+                    resides on the call stack. The height of the tree is n.
+                    Space complexity: O(n)
+Time complexity: The recursive tree has  n + n(n-1) + n(n-1)(n-2) + ..... + n! nodes 
+                    and every node except the leaf nodes do O(1) amount of work. 
+                    There are n! leaf nodes and each leaf node does
+                    O(n) work, because printing takes O(n) time. So, the 
+                    asymptotic complexity is O(n*n!)
+"""
+
+def permutations(str_):
+    permutations_helper("", str_)
+
+def permutations_helper(slate, domain):
+    if len(domain) == 0:
+        print(slate)
+        return
+
+    for index in range(len(domain)):
+        new_domain = domain[:index] + domain[index + 1:]
+        permutations_helper(slate + domain[index], new_domain)
+
+if __name__ == "__main__":
+    test_set = ["abc", "xy", "x", "aaa"]
+    for each in test_set:
+        permutations(each)
